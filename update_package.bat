ec2ad641e4560cdd3d06ece0bb56691db0df3007
Jarod Mica <jarodmica@gmail.com>
2024-01-22 00:19:18 -0800

    Add update_package.bat

---
+++ update_package.bat
@@ -0,0 +1,8 @@
+portable_git\bin\git.exe clone https://github.com/JarodMica/ai-voice-cloning.git
+xcopy ai-voice-cloning\update_package.bat /E /I /H /Y
+
+xcopy ai-voice-cloning\src src /E /I /H /Y
+
+rmdir /s /q ai-voice-cloning
+
+pause
