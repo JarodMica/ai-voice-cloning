207d9bad0fd1918bf573b7484d62a0864cc8ce2e
Jarod Mica <jarodmica@gmail.com>
2024-03-18 12:33:03 -0700

    Update the update_package.bat

--- update_package.bat
+++ update_package.bat
@@ -1,4 +1,9 @@
 portable_git\bin\git.exe clone https://github.com/JarodMica/ai-voice-cloning.git
+cd ai-voice-cloning
+git submodule init
+git submodule update --remote
+cd ..
+
 xcopy ai-voice-cloning\update_package.bat /E /I /H /Y
 
 xcopy ai-voice-cloning\src src /E /I /H /Y
@@ -6,4 +11,5 @@ xcopy ai-voice-cloning\src src /E /I /H /Y
 rmdir /s /q ai-voice-cloning
 rmdir /s /q .git
 
+@echo Finished updating!
 pause
